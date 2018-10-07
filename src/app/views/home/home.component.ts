468af193f37ef31fd164f19e841254ed4ada7c1e
Diego Santiesteban <gatodeveloper@gmail.com>
2018-10-06 22:00:47 -0500

    Added animations for homepage

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public mx : number = 0;
+  public animations : any = {};
+
+  constructor(
+    public el: ElementRef
+    ) { }
+
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
   }
+
+  scroll = (event): void => {
+    var wh = window.innerHeight;
+    var py = window.pageYOffset;
+    var scrollPosition = wh + py;
+    this.mx = (scrollPosition > this.mx) ? scrollPosition : this.mx;
+
+    console.log("Scroll Position: ", scrollPosition);
+    console.log("Max Position: ", this.mx);
+
+    if(scrollPosition >= 1180){
+      this.animations.apply1 = true;
+      this.animations.apply2 = true;
+    }
+
+    if(scrollPosition >= 1820){
+      this.animations.apply3 = true;
+      this.animations.apply4 = true;
+    }
+
+    if(scrollPosition >= 2450){
+      this.animations.apply5 = true;
+    }
+
+    if(scrollPosition >= 3070){
+      this.animations.apply6 = true;
+    }
+
+
+
+  };
 
 }
