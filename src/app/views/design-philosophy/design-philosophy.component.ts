8a32d070d324544037f8cf817a6931788212f7bc
Diego Santiesteban <gatodeveloper@gmail.com>
2018-10-06 23:26:24 -0500

    added animations for design philosophy

--- src/app/views/design-philosophy/design-philosophy.component.ts
+++ src/app/views/design-philosophy/design-philosophy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-design-philosophy',
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignPhilosophyComponent implements OnInit {
 
-  constructor() { }
+  public mx : number = 0;
+  public animations : any = {};
+
+  constructor(
+    public el: ElementRef
+    ) { }
+
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
   }
+
+  scroll = (event): void => {
+    var wh = window.innerHeight;
+    var py = window.pageYOffset;
+    var scrollPosition = wh + py;
+    this.mx = (scrollPosition > this.mx) ? scrollPosition : this.mx;
+
+    console.log("Scroll Position: ", scrollPosition);
+    console.log("Max Position: ", this.mx);
+
+    if(scrollPosition >= 1090){
+      this.animations.apply1 = true;
+    }
+
+    if(scrollPosition >= 1405){
+      this.animations.apply2 = true;
+    }
+
+    if(scrollPosition >= 1685){
+      this.animations.apply3 = true;
+    }
+
+    if(scrollPosition >= 1980){
+      this.animations.apply4 = true;
+    }
+
+
 
 }
