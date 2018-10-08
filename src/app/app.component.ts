dfa07f909e9aa2e8d5bfbcd5ce122cd2035ac325
Diego Santiesteban <gatodeveloper@gmail.com>
2018-10-07 22:47:44 -0500

    Added menu shadow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,36 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(
+    private router: Router,
+    ){
+    router.events.subscribe((event: Event) => {
+
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+        //if(event.url)
+        
+        window.scrollTo(0, 0);  
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+        // Present error to user
+        //console.log(event.error);
+      }
+      
+
+    });
+  }
+
+}
--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainHeaderComponent implements OnInit {
 
+  public showShadow;
+
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (event): void => {
+    var wh = window.innerHeight;
+    var py = window.pageYOffset;
+    var scrollPosition = wh + py;
+    this.mx = (scrollPosition > this.mx) ? scrollPosition : this.mx;
+
+    (py > 10) ? this.showShadow = true : this.showShadow = false;
+    
+
   }
 
 }
