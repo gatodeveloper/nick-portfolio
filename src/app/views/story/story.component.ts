da25389e183ecb49d6868cc867a00e09458173e5
Diego Santiesteban <gatodeveloper@gmail.com>
2018-10-07 22:01:05 -0500

    Added animations for story

--- src/app/views/story/story.component.ts
+++ src/app/views/story/story.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-story',
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoryComponent implements OnInit {
 
-  constructor() { }
+  public mx : number = 0;
+  public animations : any = {};
+
+  constructor(
+    public el: ElementRef
+    ) { }
+
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
   }
+
+  scroll = (event): void => {
+    var wh = window.innerHeight;
+    var py = window.pageYOffset;
+    var scrollPosition = wh + py;
+    this.mx = (scrollPosition > this.mx) ? scrollPosition : this.mx;
+
+    console.log("Scroll Position: ", scrollPosition);
+    console.log("Max Position: ", this.mx);
+
+    if(scrollPosition >= 820){
+      this.animations.apply1 = true;
+    }
+
+    if(scrollPosition >= 1140){
+      this.animations.apply2 = true;
+    }
+
+    if(scrollPosition >= 1340){
+      this.animations.apply3 = true;
+    }
+
+
+    if(scrollPosition >= 2080){
+      this.animations.apply4 = true;
+    }
+    if(scrollPosition >= 2400){
+      this.animations.apply5 = true;
+    }
+    if(scrollPosition >= 2725){
+      this.animations.apply6 = true;
+    }
+    if(scrollPosition >= 3065){
+      this.animations.apply7 = true;
+    }
+    if(scrollPosition >= 3390){
+      this.animations.apply8 = true;
+    }
+    if(scrollPosition >= 3730){
+      this.animations.apply9 = true;
+    }
+    if(scrollPosition >= 4190){
+      this.animations.apply10 = true;
+    }
+    if(scrollPosition >= 4580){
+      this.animations.apply11 = true;
+    }
+    
 
 }
